9215687e40bddf614d9868614b5a59e58c77fd53
syrine-ztn <82603855+syrine-ztn@users.noreply.github.com>
2022-12-31 17:14:11 +0100

    Création d’un utilisateur

--- src/main/java/com/telly/controllers/UserController.java
+++ src/main/java/com/telly/controllers/UserController.java
@@ -33,11 +33,11 @@ public class UserController {
 		return "login";
 	}
 	
-	
 	@RequestMapping("/loggedout")
 	public String showLogout() {
 		return "loggedout";
 	}
+	
 	
 	@RequestMapping("/createaccount")
 	public String createAccount(Model model, Principal principal) {
@@ -63,5 +63,40 @@ public class UserController {
 
 	}
 
+	@RequestMapping(value = "/reservebook", method = RequestMethod.POST)
+	public String createReserveBook(@Validated(FormValidationGroup.class) Reserve reserve, BindingResult result, Principal principal) {
+		
+		if (result.hasErrors()) {
+			return "reservebus";
+		}
+		
+		String username = principal.getName();
+		reserve.getUser().setUsername(username);
+		
+		reserveService.reserve(reserve);
+	
+		
+		return "home";
+
+	}
+	
+	@RequestMapping(value = "/getreservations", method = RequestMethod.GET)
+	public String getReserveBook(@Validated(FormValidationGroup.class) Reserve reserve, Model model, Principal principal) {
+		
+		
+		String username = principal.getName();
+		reserve.getUser().setUsername(username);
+		
+		List<Reserve> reserves = reserveService.getReserves(username);
+		model.addAttribute("reserves", reserves);
+		System.out.println(reserves);
+	
+		
+		return "home";
+
+	}
+	
 
 }
+
+
