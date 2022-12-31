d3593428555674b7d362fad15f0f22e64a047aa5
syrine-ztn <82603855+syrine-ztn@users.noreply.github.com>
2022-12-31 15:05:29 +0100

    Affichage des réservations

--- src/main/java/com/telly/controllers/UserController.java
+++ src/main/java/com/telly/controllers/UserController.java
@@ -46,7 +46,23 @@ public class UserController {
 
 	}
 	
+    @RequestMapping(value = "/getreservations", method = RequestMethod.GET)
+	public String getReserveBook(@Validated(FormValidationGroup.class) Reserve reserve, Model model, Principal principal) {
+		
+		
+		String username = principal.getName();
+		reserve.getUser().setUsername(username);
+		
+		List<Reserve> reserves = reserveService.getReserves(username);
+		model.addAttribute("reserves", reserves);
+		System.out.println(reserves);
+	
+		
+		return "home";
 
+	}
+	
+	
 }
 
 
